0d15fb7ba365ee872459f0d29f09337db46be575
mastalju <mastalju@sheridancollege.ca>
2024-09-24 22:12:09 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -60,6 +60,9 @@ public class CardTrick {
             System.out.println("Sorry, your card is not in the magic hand");
         }
         // add one luckcard hard code 2,clubs
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
     }
     
 }
